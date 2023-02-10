398f6b81488795528b054a8fa392699e7f1c24ea
leeteayu <ehdrkq2005@naver.com>
2023-02-10 13:44:58 +0900

    section1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-$(function(){
-    
-})
+// $(function(){
+//     var nightSwiper = new Swiper(".night_swiper", {
+//         spaceBetween:0,
+//         speed:800,
+//         centeredSlides: true,
+//         infinite:true,
+//         loop:true,
+//         autoplay: {
+//         delay: 3000,
+//         disableOnInteraction: false,
+//         },
+//         // breakpoints: {
+//         // 768: {
+//         // slidesPerView: 2,  //브라우저가 768보다 클 때
+//         // spaceBetween: 0,
+//         // centeredSlides:false,
+//         // },
+//         // 950: {
+//         // slidesPerView: 3,  //브라우저가 768보다 클 때
+//         // spaceBetween: 0,
+//         // centeredSlides:false,
+//         // },
+//         // 1280: {
+//         // slidesPerView: 4,  //브라우저가 1278보다 클 때
+//         // spaceBetween: 20,
+//         // centeredSlides:false,
+//         // },
+//         // },
+//     });
+// })
