b73e6df43ad98944b424bbdb88062d7d98f0406a
leeteayu <ehdrkq2005@naver.com>
2023-02-15 16:39:29 +0900

    23.02.15

--- js/common.js
+++ js/common.js
@@ -1,3 +1,36 @@
 $(function(){
-    
+    $(".depth1 > li > a").mouseenter(function(){
+        $(this).parent().siblings().find("a").removeClass("on");
+        $(".hd_wrap").removeClass("active");
+        $(".depth2").removeClass("boxon");
+    });
+    $(".depth_on1").mouseenter(function(){
+        $(this).parent().parent().siblings(".depth2").removeClass("boxon");
+        $(this).addClass("on");
+        $(".hd_wrap").addClass("active");
+        $(this).parent().parent().siblings(".depth2_1").addClass("boxon");
+    });
+    $(".depth_on2").mouseenter(function(){
+        $(this).parent().parent().siblings(".depth2").removeClass("boxon");
+        $(this).addClass("on")
+        $(".hd_wrap").addClass("active");
+        $(this).parent().parent().siblings(".depth2_2").addClass("boxon");
+    });
+    $(".depth_on5").mouseenter(function(){
+        $(this).parent().parent().siblings(".depth2").removeClass("boxon");
+        $(this).addClass("on")
+        $(".hd_wrap").addClass("active");
+        $(this).parent().parent().siblings(".depth2_5").addClass("boxon");
+    });
+    $(".depth2").mouseleave(function(){
+        $(".depth1 > li > a").removeClass("on");
+        $(".hd_wrap").removeClass("active");
+        $(".depth2").removeClass("boxon");
+    });
+
+
+    $(".ham_on").click(function(){
+        $("body").toggleClass("hidden");
+        $(".site_container").toggleClass("siteon");
+    })
 })
