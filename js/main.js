179ec48e858c44246dffbf3413ab110ec9494edf
leeteayu <ehdrkq2005@naver.com>
2023-02-15 11:51:21 +0900

    23.02.15

--- js/main.js
+++ js/main.js
@@ -9,30 +9,13 @@ $(function(){
         fadeEffect: { 
             crossFade: true 
         },
-        // autoplay: {
-        // delay: 5000,
-        // disableOnInteraction: false,
-        // },
+        autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+        },
         navigation: {
             nextEl: ".next",
             prevEl: ".prev",
         },
-        // breakpoints: {
-        // 768: {
-        // slidesPerView: 2,  //브라우저가 768보다 클 때
-        // spaceBetween: 0,
-        // centeredSlides:false,
-        // },
-        // 950: {
-        // slidesPerView: 3,  //브라우저가 768보다 클 때
-        // spaceBetween: 0,
-        // centeredSlides:false,
-        // },
-        // 1280: {
-        // slidesPerView: 4,  //브라우저가 1278보다 클 때
-        // spaceBetween: 20,
-        // centeredSlides:false,
-        // },
-        // },
     });
 })
