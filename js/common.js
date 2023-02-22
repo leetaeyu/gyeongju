6bc344596a37f4e16f5a13a4b9bcc0a088d7b7dc
leeteayu <ehdrkq2005@naver.com>
2023-02-22 10:29:50 +0900

    23.02.22

--- js/common.js
+++ js/common.js
@@ -1,4 +1,37 @@
 $(function(){
+    let wH = window.innerHeight;
+    const hd = $(".visual_label_pc");
+    let hdH = hd.height();  // inner (패딩) outer (보더)
+
+    function reset() {
+        hdH = hd.height();
+    }
+
+    $(document).scroll(function(){
+        var scroll_bar = $(document).scrollTop();
+        var fix_start = $(".visual_container").height() - 60;
+        var fix_middle = $(".video_container").height();
+        var fix_end =  $(".sns_container")[0].offsetTop + 2400;
+        var notice_fix = $(".visual_label_pc");
+        if(scroll_bar >= fix_start & scroll_bar <= fix_end){
+            notice_fix.addClass("fixed")
+            if(scroll_bar >= fix_middle){
+                notice_fix.animate({"bottom":"0"},250)
+            }
+        }else{
+            notice_fix.removeClass("fixed")
+        }
+        $(".ani_top, .ani_left").each(function(){
+            let offsetTop = $(this).offset().top - wH;
+            //console.log(offsetTop);
+            if(scroll_bar > offsetTop) {
+                $(this).addClass("fade_in");
+            } else {
+                $(this).removeClass("fade_in");
+            }
+        });
+    });
+
     $(".depth1 > li > a").mouseenter(function(){
         $(this).parent().siblings().find("a").removeClass("on");
         $(".hd_wrap").removeClass("active");
@@ -31,18 +64,15 @@ $(function(){
         $("body").toggleClass("hidden");
         $(".site_container").toggleClass("siteon");
     })
-
     $(".mo_depth1 > a").click(function(){
         $(this).parent().parent().siblings().find(".mo_depth2").stop().slideUp(400);
         $(this).next().stop().slideToggle(400);
     });
-
     $(".mo_ham_on").click(function(){
         $("body").toggleClass("hidden");
         $(".modal").toggleClass("modal_on")
         $(".mo_site_wrap").toggleClass("mo_right");
     })
-
     $("a").click(function(){
         if($(this).attr("href") == "#") {
             return false;
--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 $(function(){
-    let wH = window.innerHeight;
-    let scTop = 0;
-
-
     var nightSwiper = new Swiper(".night_swiper", {
         spaceBetween:0,
         speed:800,
@@ -25,16 +21,5 @@ $(function(){
 
 
 
-    $(document).scroll(function(){
-        scTop = $(document).scrollTop();
-        $(".ani_top, .ani_left, .visual_label_pc").each(function(){
-            let offsetTop = $(this).offset().top - wH;
-            //console.log(offsetTop);
-            if(scTop > offsetTop) {
-                $(this).addClass("fade_in");
-            } else {
-                $(this).removeClass("fade_in");
-            }
-        });
-    });
+
 })
